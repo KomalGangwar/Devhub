c5792bcdc7e9d6da63d4d5251e61d06821202aa5
Komal  Gangwar <107317696+KomalGangwar@users.noreply.github.com>
2024-12-21 20:05:44 +0530

    Add files via upload

---
+++ frontend/src/components/Home.js
@@ -0,0 +1,19 @@
+import React from 'react'
+
+const Home = () => {
+  return (
+    <div>
+      <div className='Header'>
+          <h1>Devhub</h1>
+          <div className='Header-rights'>
+          <h3>Home</h3>
+          <h3>Explore</h3>
+          <h3>Profile</h3>
+          <h3>Dashboard</h3>
+          </div>
+        </div> 
+    </div>
+  )
+}
+
+export default Home

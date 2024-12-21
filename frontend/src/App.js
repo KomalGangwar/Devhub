d5e9371309931b7e7f1f9ba7dff41517a38d1d64
Komal  Gangwar <107317696+KomalGangwar@users.noreply.github.com>
2024-12-21 20:06:35 +0530

    Update App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,8 +1,17 @@
 import React from 'react'
+import './App.css'
 
 const App = () => {
   return (
-    <div>App</div>
+    <div>  
+        <div className='Content'>
+        <h1>Login</h1>
+          <div className='Content-box'>
+              <input type='email' placeholder='Enter your email' />
+              <input type='github' placeholder='Enter your github username' />
+          </div>
+        </div>
+     </div>
   )
 }
 

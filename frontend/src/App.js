a06dbf15ef0a4466f0c03f7e08b5a1045b4c2569
Komal  Gangwar <107317696+KomalGangwar@users.noreply.github.com>
2025-02-11 13:14:59 +0530

    Update App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,17 +1,38 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './App.css'
+// import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+// import SignUp from './components/SignUp';
+// import Login from './components/Login';
+// import Dashboard from './components/Dashboard';
 
 const App = () => {
+  const[isLogin,setIsLogin]=useState(true);
   return (
-    <div>  
-        <div className='Content'>
-        <h1>Login</h1>
-          <div className='Content-box'>
-              <input type='email' placeholder='Enter your email' />
-              <input type='github' placeholder='Enter your github username' />
-          </div>
-        </div>
-     </div>
+  //   <Router>
+  //   <Routes>
+  //   <Route path="/signup" element={<SignUp />} />
+  //   <Route path="/login" element={<Login />} />
+  //   <Route path="/dashboard" element={<Dashboard />} />
+  // </Routes>
+  // </Router>
+  <div>  
+  <div className='Content'>
+    <div className='Content-box'>
+      <h1>{isLogin ? "Login" : "Sign Up"}</h1>
+      <form>
+        <input type='email' placeholder='Enter your email' />
+        <input type='password' placeholder='Password' />
+        <button>{isLogin ? "Login" : "Sign Up"}</button>
+      </form>
+      <button>Sign in with Github</button>
+      <p>
+        {isLogin? "Don't have an account?" : "Already have ab account?"}
+        <span onClick={()=>setIsLogin(!isLogin)}> {isLogin ? "Login" : "Sign Up"}</span>
+      </p>
+    </div>
+  </div>
+</div>
+
   )
 }
 
